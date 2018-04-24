590fe1936e9a10084c788fd85b85801bee1ed0b9
ttsdesign <ttsdesign@gmail.com>
2018-04-24 13:22:37 -0600

    Update Array.js

--- src/primitives/Array.js
+++ src/primitives/Array.js
@@ -1,22 +1,43 @@
 
-if (!("Contains" in Array.prototype)) {
-	Array.prototype.Contains = Array.prototype.includes;
-}
+(function(NS) {
 
-if (!("Unique" in Array.prototype)) {
-	Array.prototype.Unique = function Unique () {
-		var u = [];
-		this.forEach(function (e, i) {
-			if (!u.Contains(e)) {
-				u.push(e);
+	if (!Array.prototype.hasOwnProperty("Contains")) {
+		Object.defineProperty(Array.prototype, "Contains", {
+			configurable: false,
+			enumerable: false,
+			value: function(e) {
+				return this.includes(e);
 			}
 		});
-		return u;
 	}
-}
 
-if (typeof IsArray === "undefined") {
-	IsArray = Array.isArray || function (a) {
-		return !!a && '[object Array]' == Object.prototype.toString.call(a)
+	if (!Array.prototype.hasOwnProperty("Unique")) {
+		Object.defineProperty(Array.prototype, "Unique", {
+			configurable: false,
+			enumerable: false,
+			value: function() {
+				var u = [];
+				this.forEach(function (e, i) {
+					if (!u.Contains(e)) {
+						u.push(e);
+					}
+				});
+				return u;
+			}
+		});
 	}
-}
+
+	if (!Array.hasOwnProperty('IsArray')) {
+		Object.defineProperty(NS, 'IsArray', {
+			configurable: false,
+			enumerable: false,
+			value: function(a) {
+				if ("isArray" in Array) {
+					return Array.isArray(a);
+				}
+				return !!a && '[object Array]' == Object.prototype.toString.call(a)
+			}
+		});
+	}
+
+}(typeof window !== "undefined" ? window : (typeof global !== "undefined") ? global : this));
