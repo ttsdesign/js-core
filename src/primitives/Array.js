979a9d629d46ef80d70239940c9fedc1a043bc85
ttsdesign <ttsdesign@gmail.com>
2018-04-24 07:29:53 -0600

    Update Array.js

--- src/primitives/Array.js
+++ src/primitives/Array.js
@@ -1,27 +1,22 @@
-"use strict";
 
-(function(NS) {
+if (!("Contains" in Array.prototype)) {
+	Array.prototype.Contains = Array.prototype.includes;
+}
 
-	if (!("Contains" in NS.Array.prototype)) {
-		NS.Array.prototype.Contains = NS.Array.prototype.includes;
+if (!("Unique" in Array.prototype)) {
+	Array.prototype.Unique = function Unique () {
+		var u = [];
+		this.forEach(function (e, i) {
+			if (!u.Contains(e)) {
+				u.push(e);
+			}
+		});
+		return u;
 	}
+}
 
-	if (!("Unique" in NS.Array.prototype)) {
-		NS.Array.prototype.Unique = function Unique () {
-			var u = [];
-			this.forEach(function (e, i) {
-				if (!u.Contains(e)) {
-					u.push(e);
-				}
-			});
-			return u;
-		}
+if (typeof IsArray === "undefined") {
+	IsArray = Array.isArray || function (a) {
+		return !!a && '[object Array]' == Object.prototype.toString.call(a)
 	}
-
-	if (!("IsArray" in NS)) {
-		NS.IsArray = NS.Array.isArray || function (a) {
-			return !!a && '[object Array]' == Object.prototype.toString.call(a)
-		}
-	}
-
-}(typeof window !== "undefined" ? window : (typeof global !== "undefined") ? global : this));
+}
