2e50dabf996d57ea242112128b517c79cd5204af
TTS Design <ttsdesign@gmail.com>
2018-04-25 16:32:41 -0600

    -

--- src/primitives/Object/Type.js
+++ src/primitives/Object/Type.js
@@ -7,3 +7,13 @@ if (!Object.prototype.hasOwnProperty('Type')) {
 		}
 	});
 }
+
+if (!Object.hasOwnProperty('Type')) {
+	Object.defineProperty(Object, 'Type', {
+		configurable: false,
+		enumerable: false,
+		value: function(v) {
+			return Object.prototype.toString.call(v).split(" ")[1].replace("]", "");
+		}
+	});
+}
