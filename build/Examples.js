e007426579aeec976caae3c72166503d11c3b9ca
TTS Design <ttsdesign@gmail.com>
2018-05-17 08:43:37 -0600

    .

--- build/Examples.js
+++ build/Examples.js
@@ -9,3 +9,17 @@ Fs.readdirSync("examples").forEach(function (f) {
 	}
 });
 
+var template = Fs.readFileSync("dist/Examples/index.template.html", "utf8");
+var menus = [];
+Fs.readdirSync("dist/Examples").forEach(function (f) {
+	if (Fs.statSync("dist/Examples/"+f).isFile()) {
+		if (!f.Contains(".template") && !f.endsWith(".js") && f != "REPL.html" && f != "home.html" && f != "index.html") {
+			menus.push('<a href="'+f+'" target="main">'+f.replace(/\.html$/, "")+'</a>');
+		}
+	}
+});
+
+template = template.replace(/\<\!--\$MENU\$--\>/, menus.join("\r\n<br>\r\n"));
+Fs.writeFileSync("dist/Examples/index.html", template.replace(/\<\!--\$MENU\$--\>/, menus.join("\r\n<br>\r\n")), "utf8");
+
+
